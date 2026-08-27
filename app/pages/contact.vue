7873484c33a1e940858cff2f03060b47f42946dd
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-08-27 23:58:56 +0300

    feat(contact): add contact page hero section

---
+++ app/pages/contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <SharedPageHero
+    background-image="/img/contact/contact-us.jpg"
+    tagline="Contact Us"
+    headline="Let's Start a<br />"
+    headline-highlight="Conversation."
+    paragraph="Have a project in mind? Looking for technology solutions that deliver results? Reach out to us and let's discuss how SCINTL can help you achieve your goals."
+    :overlay-opacity="60"
+  />
+</template>
