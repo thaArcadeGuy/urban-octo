a8ef2de592adb27d3380e0990a5508d5b37eb482
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-08-30 00:32:26 +0300

    feat(careers): add careers page hero section

---
+++ app/pages/careers.vue
@@ -0,0 +1,11 @@
+<template>
+  <SharedPageHero
+    background-image="/img/careers/careers.jpg"
+    tagline="Join Our Team"
+    headline="Build Your<br />"
+    headline-highlight="Career with SCINTL"
+    paragraph="We're looking for passionate talent to help us deliver technology that transforms businesses and communities across Africa."
+    :overlay-opacity="60"
+  />
+ 
+</template>
