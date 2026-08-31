c26ec4499a8f504c6a01593639f21fcbcdb5f69c
thaArcadeGuy <gilbertsilver64@gmail.com>
2026-08-31 13:03:54 +0300

    feat(solutions): add solutions page hero section and CTA

---
+++ app/pages/solutions.vue
@@ -0,0 +1,11 @@
+<template>
+  <SharedPageHero
+    background-image="/img/solutions/solutions-hero.jpg"
+    tagline="What We Do"
+    headline="Technology Solutions That Drive Business Growth<br />"
+    headline-highlight="& Deliver Real Results"
+    paragraph="From cloud infrastructure and cybersecurity to enterprise systems and implementation, we deliver tailored technology solutions that solve real business problems and accelerate your return on investment."
+    :overlay-opacity="60"
+  />
+  <SharedCta />
+</template>
